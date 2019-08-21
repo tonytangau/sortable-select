b5c4f75719f29d6897776b8c24db1a2236993cdd
Tony Tang <tony.cn@outlook.com>
2019-08-21 16:14:00 +0800

    Add react-select

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
 function App() {
+  const [selected, setSelected] = React.useState([options[2]]);
+
+  const handleChange = (selectedOptions) => setSelected(selectedOptions);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <Select
+          isMulti
+          options={options}
+          value={selected}
+          onChange={handleChange}
+          closeMenuOnSelect={false}
+        />
+      </div>
     </div>
   );
 }
