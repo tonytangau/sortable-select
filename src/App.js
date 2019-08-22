4f00e3ba78265fbcc60f663b058b307d9caa3d34
Tony Tang <tony.cn@outlook.com>
2019-08-22 10:53:09 +0800

    Add drag and drop using react-sortable-hoc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import arrayMove from 'array-move';
+import Select, { components } from 'react-select';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import './App.css';
 
-import Select from 'react-select';
+const SortableMultiValue = SortableElement(components.MultiValue);
+const SortableSelect = SortableContainer(Select);
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
@@ -12,17 +16,24 @@ const options = [
 function App() {
   const [selected, setSelected] = React.useState([options[2]]);
 
-  const handleChange = (selectedOptions) => setSelected(selectedOptions);
+  const onChange = (selectedOptions) => setSelected(selectedOptions);
+  const onSortEnd = ({ oldIndex, newIndex })=> setSelected(arrayMove(selected, oldIndex, newIndex));
 
   return (
     <div className="App">
       <div className="Container">
-        <Select
+        <SortableSelect
           isMulti
           options={options}
           value={selected}
-          onChange={handleChange}
+          onChange={onChange}
+          components={{ MultiValue: SortableMultiValue }}
           closeMenuOnSelect={false}
+          axis="xy"
+          onSortEnd={onSortEnd}
+          distance={5}
+          lockToContainerEdges={true}
+          getHelperDimensions={({ node })=> node.getBoundingClientRect()}
         />
       </div>
     </div>
